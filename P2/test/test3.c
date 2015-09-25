8be57b206aca836f5f15f85244cbf625b7cb5252
yaokai1117 <yaokai1117@gmail.com>
2015-09-25 17:05:59 +0800

    P2 submit

--- P2/test/test3.c
+++ P2/test/test3.c
@@ -4,16 +4,22 @@ int a[10];
 void sort()
 {
 	int i, j, key;
+	int flag;
 	i = 0;
 	j = 0;
 	key = 0;
+	flag = 1;
 
 	while (i < 10) {
 		j = i - 1;
 		key = a[i];
-		while ((j > 0) * (a[j] > key)) {
-			a[j+1] = a[j];
-			j = j - 1;
+		while (j > 0) {
+			if (a[j] <= key)
+				flag = 0;
+			if (flag) {
+				a[j+1] = a[j];
+				j = j - 1;
+			}
 		}		
 		a[j] = key;
 		i = i + 1;
